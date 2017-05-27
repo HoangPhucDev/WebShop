a84a77c6b029a1041b21457bd2a297131f6e7613
HoangPhucDev <nhp1107@gmail.com>
2017-05-27 15:02:13 +0700

    hoan thanh co ban quan ly don hang

---
+++ WebShop/Areas/Admin/Controllers/CustomerController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ModelWebBanHang;
+
+namespace WebShop.Areas.Admin.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private WebBanHang db = new WebBanHang();
+
+        // GET: Admin/Customer
+        public ActionResult Index()
+        {
+            var kHACH_HANG = db.KHACH_HANG.Include(k => k.THANH_VIEN);
+            return View(kHACH_HANG.ToList());
+        }
+
+        // GET: Admin/Customer/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            KHACH_HANG kHACH_HANG = db.KHACH_HANG.Find(id);
+            if (kHACH_HANG == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kHACH_HANG);
+        }
+
+        // GET: Admin/Customer/Create
+        public ActionResult Create()
+        {
+            ViewBag.MA_TV = new SelectList(db.THANH_VIEN, "MA_TV", "MAT_KHAU");
+            return View();
+        }
+
+        // POST: Admin/Customer/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MA_KH,MA_TV,TEN_KH,GIOI_TINH,NGAY_SINH,TINH,DIA_CHI,LIEN_HE,Email")] KHACH_HANG kHACH_HANG)
+        {
+            if (ModelState.IsValid)
+            {
+                db.KHACH_HANG.Add(kHACH_HANG);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MA_TV = new SelectList(db.THANH_VIEN, "MA_TV", "MAT_KHAU", kHACH_HANG.MA_TV);
+            return View(kHACH_HANG);
+        }
+
+        // GET: Admin/Customer/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            KHACH_HANG kHACH_HANG = db.KHACH_HANG.Find(id);
+            if (kHACH_HANG == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MA_TV = new SelectList(db.THANH_VIEN, "MA_TV", "MAT_KHAU", kHACH_HANG.MA_TV);
+            return View(kHACH_HANG);
+        }
+
+        // POST: Admin/Customer/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MA_KH,MA_TV,TEN_KH,GIOI_TINH,NGAY_SINH,TINH,DIA_CHI,LIEN_HE,Email")] KHACH_HANG kHACH_HANG)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(kHACH_HANG).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MA_TV = new SelectList(db.THANH_VIEN, "MA_TV", "MAT_KHAU", kHACH_HANG.MA_TV);
+            return View(kHACH_HANG);
+        }
+
+        // GET: Admin/Customer/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            KHACH_HANG kHACH_HANG = db.KHACH_HANG.Find(id);
+            if (kHACH_HANG == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kHACH_HANG);
+        }
+
+        // POST: Admin/Customer/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            KHACH_HANG kHACH_HANG = db.KHACH_HANG.Find(id);
+            db.KHACH_HANG.Remove(kHACH_HANG);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebShop/Areas/Admin/Controllers/InvoiceController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ModelWebBanHang;
+
+namespace WebShop.Areas.Admin.Controllers
+{
+    public class InvoiceController : Controller
+    {
+        private WebBanHang db = new WebBanHang();
+
+        // GET: Admin/Invoice
+        public ActionResult Index()
+        {
+            var hOA_DON = db.HOA_DON.Include(h => h.KHACH_HANG).Include(h => h.NHAN_VIEN);
+            return View(hOA_DON.ToList());
+        }
+
+        // GET: Admin/Invoice/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ViewBag.Order = db.HOA_DON.Find(id);
+            if (db.HOA_DON.Find(id) == null)
+            {
+                return HttpNotFound();
+            }
+            
+            List<CHITIET_HD> hOA_DON = db.CHITIET_HD.Where(p => p.MA_HD == id).ToList();
+            
+            return View(hOA_DON);
+        }
+
+        // GET: Admin/Invoice/Create
+        public ActionResult Create()
+        {
+            ViewBag.MA_KH = new SelectList(db.KHACH_HANG, "MA_KH", "MA_TV");
+            ViewBag.MA_NV = new SelectList(db.NHAN_VIEN, "MA_NV", "MAT_KHAU");
+            return View();
+        }
+
+        // POST: Admin/Invoice/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MA_HD,MA_KH,MA_NV,NGAY_LAP,TINH_GIAO_HANG,DIA_CHI_GIAO_HANG,GHI_CHU,PHI_GIAO_HANG,THANH_TIEN,TRANG_THAI")] HOA_DON hOA_DON)
+        {
+            if (ModelState.IsValid)
+            {
+                db.HOA_DON.Add(hOA_DON);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MA_KH = new SelectList(db.KHACH_HANG, "MA_KH", "MA_TV", hOA_DON.MA_KH);
+            ViewBag.MA_NV = new SelectList(db.NHAN_VIEN, "MA_NV", "MAT_KHAU", hOA_DON.MA_NV);
+            return View(hOA_DON);
+        }
+
+        // GET: Admin/Invoice/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            HOA_DON hOA_DON = db.HOA_DON.Find(id);
+            if (hOA_DON == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MA_KH = new SelectList(db.KHACH_HANG, "MA_KH", "MA_TV", hOA_DON.MA_KH);
+            ViewBag.MA_NV = new SelectList(db.NHAN_VIEN, "MA_NV", "MAT_KHAU", hOA_DON.MA_NV);
+            return View(hOA_DON);
+        }
+
+        // POST: Admin/Invoice/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MA_HD,MA_KH,MA_NV,NGAY_LAP,TINH_GIAO_HANG,DIA_CHI_GIAO_HANG,GHI_CHU,PHI_GIAO_HANG,THANH_TIEN,TRANG_THAI")] HOA_DON hOA_DON)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(hOA_DON).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MA_KH = new SelectList(db.KHACH_HANG, "MA_KH", "MA_TV", hOA_DON.MA_KH);
+            ViewBag.MA_NV = new SelectList(db.NHAN_VIEN, "MA_NV", "MAT_KHAU", hOA_DON.MA_NV);
+            return View(hOA_DON);
+        }
+
+        // GET: Admin/Invoice/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            HOA_DON hOA_DON = db.HOA_DON.Find(id);
+            if (hOA_DON == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hOA_DON);
+        }
+
+        // POST: Admin/Invoice/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            HOA_DON hOA_DON = db.HOA_DON.Find(id);
+            db.HOA_DON.Remove(hOA_DON);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
